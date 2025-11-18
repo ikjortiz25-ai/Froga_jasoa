ca2c5a00518cf16d427a9432b2c7cb3b08f924ed
ikjortiz25 <ikjortiz25@example.com>
2025-11-18 09:16:45 +0100

    1.commit

---
+++ src/EstekakEtaScrumAzalpena.java
@@ -0,0 +1,10 @@
+public class EstekakEtaScrumAzalpena {
+    public static void main(String[] args) {
+        System.out.println("Scrum metodologia(https://trello.com/b/OT1zb0ZB/proba-jasoa):");
+        System.out.println("Metodologia hau metodologia arinetatik erabilgarriena da.");
+        System.out.println("Metodologia honetan sprint laburrak egiten dira.");
+        System.out.println("Lan egiteko modu honi esker nagusiak parte hartze handiagoa da feedback asko dagoelako.");
+        System.out.println("");
+
+    }
+}
