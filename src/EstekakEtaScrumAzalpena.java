9e92000d97a9ad6d8a35b1f5f74c37e3e128cfe4
ikjortiz25 <ikjortiz25@example.com>
2025-11-18 09:32:44 +0100

    Estekak eta scrum azalpena amaituta

--- src/EstekakEtaScrumAzalpena.java
+++ src/EstekakEtaScrumAzalpena.java
@@ -1,10 +1,14 @@
+
 public class EstekakEtaScrumAzalpena {
     public static void main(String[] args) {
         System.out.println("Scrum metodologia(https://trello.com/b/OT1zb0ZB/proba-jasoa):");
         System.out.println("Metodologia hau metodologia arinetatik erabilgarriena da.");
         System.out.println("Metodologia honetan sprint laburrak egiten dira.");
         System.out.println("Lan egiteko modu honi esker nagusiak parte hartze handiagoa da feedback asko dagoelako.");
-        System.out.println("");
-
+        System.out.println("Bertan rol garrantzitsuena scrum master deiturikoa da, honek ritmoa eta egin beharrekoak markatuko ditu.");
+        System.out.println("Gainera egun bakoitzero bilerak egingo dira:");
+        System.out.println("Egindakoa, Egun bertan egin baharrekoa eta Aurrerago egin beharrekoaz hitz egiteko.");
+        System.out.println("Amaitzeko Sistema honek puntuazioen sistema bat du honi esker jakin dezakegu ataza bakoitzak zenbat denbora eramango digun(Fibonacci)");
+        System.out.println("Link github: https://github.com/ikjortiz25-ai/Froga_jasoa.git");
     }
 }
